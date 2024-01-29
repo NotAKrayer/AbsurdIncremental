a4f67429bab69a3104212527327e9068e1de38ea
NotAKrayer <157698427+NotAKrayer@users.noreply.github.com>
2024-01-29 23:29:56 +0500

    Update 0.2
    new generator.
    bugfixes

--- scripts/NothingGen.js
+++ scripts/NothingGen.js
@@ -3,6 +3,12 @@ var gen1 = new Decimal("0.0");
 var gen1val = new Decimal("0.0");
 var gen1x = new Decimal("1.0");
 
+var gen2cost = new Decimal("100.0");
+var gen2 = new Decimal("0.0");
+var gen2val = new Decimal("0.0");
+var gen2x = new Decimal("1.0");
+var gen1get = new Decimal("1.0");
+
 function BGButtonTest() {
   if (Nothing.gte(gen1cost)) {
     const gen1bg = document.getElementById("gen1bg");
@@ -15,17 +21,29 @@ function BGButtonTest() {
   }
 }
 
+function BGButtonTest2() {
+  if (Nothing.gte(gen2cost)) {
+    const gen2bg = document.getElementById("gen2bg");
+    gen2bg.style.background = "#43c443";
+    gen2bg.style.cursor = "pointer";
+  } else if (Nothing.lt(gen2cost)) {
+    const gen2bg = document.getElementById("gen2bg");
+    gen2bg.style.background = "transparent";
+    gen2bg.style.cursor = "default";
+  }
+}
+
 function NothingGet() {
   Nothing = Decimal.add(Nothing, nget);
   document.getElementById("Nothing").innerHTML = Nothing.toPrecision(3);
 }
 
 setInterval("BGButtonTest()", 100);
+setInterval("BGButtonTest2()", 100);
 setInterval("NothingGet()", 1000);
 
 function gen1Buy() {
   if (Nothing.gte(gen1cost)) {
-    nget = Decimal.add(nget, "1");
     gen1 = Decimal.add(gen1, "1");
     gen1val = Decimal.add(gen1val, "1");
     Nothing = Decimal.minus(Nothing, gen1cost);
@@ -33,8 +51,24 @@ function gen1Buy() {
     document.getElementById("gen1").innerHTML = gen1.toPrecision(3);
 	document.getElementById("gen1cost").innerHTML = gen1cost.toPrecision(3);
     document.getElementById("gen1val").innerHTML = gen1val.toPrecision(3);
+	const gen2bg = document.getElementById("gen2bg");
+    gen2bg.style.display = "block";
   }
 }
+
+function gen2Buy() {
+  if (Nothing.gte(gen2cost)) {
+    nget = Decimal.add(nget, "1");
+    gen2 = Decimal.add(gen2, "1");
+    gen2val = Decimal.add(gen2val, "1");
+    Nothing = Decimal.minus(Nothing, gen2cost);
+    document.getElementById("Nothing").innerHTML = Nothing.toPrecision(3);
+    document.getElementById("gen2").innerHTML = gen2.toPrecision(3);
+	document.getElementById("gen2cost").innerHTML = gen2cost.toPrecision(3);
+    document.getElementById("gen2val").innerHTML = gen2val.toPrecision(3);
+  }
+}
+
 
 function newGen1() {
   if (new Decimal(gen1).gte(10.0)) {
@@ -52,4 +86,33 @@ function newGen1() {
   }
 }
 
+function newGen2() {
+  if (new Decimal(gen2).gte(10.0)) {
+    gen2x = Decimal.mul(gen2x, "2");
+    gen2cost = Decimal.mul(gen2cost, "100");
+    gen2 = "0.0";
+    nget = Decimal.mul(nget, gen2x);
+	document.getElementById("gen2cost").innerHTML = gen2cost.toPrecision(3);
+    document.getElementById("gen2x").innerHTML = gen2x.toPrecision(3);
+    document.getElementById("gen2").innerHTML = new Decimal(gen2).toPrecision(3);
+    document.getElementById("gen2val").innerHTML = gen2val.toPrecision(3);
+    const gen2bg = document.getElementById("gen2bg");
+    gen2bg.style.background = "transparent";
+    gen2bg.style.cursor = "default";
+  }
+}
+
+function gennget() {
+	nget = Decimal.mul(gen1val, gen1x);
+	gen1get = Decimal.mul(gen2val, gen2x);
+}
+
+function gen2nget() {
+	gen1val = Decimal.add(gen1val, gen1get);
+	document.getElementById("gen1val").innerHTML = gen1val.toPrecision(3);
+}
+
+setInterval("gennget()", 100);
+setInterval("gen2nget()", 1000);
 setInterval("newGen1()", 100);
+setInterval("newGen2()", 100);
